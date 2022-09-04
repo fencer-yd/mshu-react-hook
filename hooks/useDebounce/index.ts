57b3310438b006ca9e7e211533240a5570415717
fencer <982039620@qq.com>
2022-09-04 13:33:45 +0800

    Update index.ts

--- hooks/useDebounce/index.ts
+++ hooks/useDebounce/index.ts
@@ -4,6 +4,7 @@ function useDebounce<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
+    // keep default 60HZ 
     const timer = setTimeout(() => setDebouncedValue(value), delay || 16);
 
     return () => {
